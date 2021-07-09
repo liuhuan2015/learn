68a0aa3b0329c82f5f9ce3c757afcc52457aab03
80003770 <80003770@sfmail.sf-express.com>
2021-07-10 01:01:21 +0800

    Drawable在自定义View时的使用（Drawable，只进行绘制）

---
+++ app/src/main/java/com/liuh/learn/DrawableView.kt
@@ -0,0 +1,19 @@
+package com.liuh.learn
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.util.AttributeSet
+import android.view.View
+
+class DrawableView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private val drawable = ColorDrawable(Color.RED)
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawable.setBounds(50, 50, width, height)
+        drawable.draw(canvas)
+    }
+}
